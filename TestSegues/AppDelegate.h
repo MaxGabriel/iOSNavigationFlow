c027608aab29f2961a06026599e4239af6c2b699
Maximilian Tagher <feedback.tagher@gmail.com>
2012-12-15 22:33:21 -0800

    Initial Commit

---
+++ TestSegues/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestSegues
+//
+//  Created by Maximilian Tagher on 12/15/12.
+//  Copyright (c) 2012 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
